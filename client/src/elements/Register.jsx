8352d28afd09510695f9490e3a3e952914c0244a
Panagiotis-Oikonomou <panosoikonomou04@gmail.com>
2026-03-18 17:31:27 +0200

    Completed Register Form

--- client/src/elements/Register.jsx
+++ client/src/elements/Register.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios, { all } from 'axios';
 import { Link, useAsyncError, useNavigate } from "react-router-dom";
 import styles from './LoginRegister.module.css';
@@ -16,22 +16,35 @@ function Register() {
         username: '',
         password: ''
     });
-    const [conPass, setConPass] = useState({cpsw: ""});
+    const [conPass, setConPass] = useState({ cpsw: "" });
+
+    const [providers, setProviders] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:5000/get_providers')
+            .then((res) => {
+                setProviders(res.data);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }, []);
 
     const [errors, setErrors] = useState({
         fname: "",
         lname: "",
         clock: "",
         username: "",
+        email: "",
         password: "",
         pswmatch: ""
     });
 
-    const [cpswError, setCpswError] = useState({cpsw: ""});
+    const [cpswError, setCpswError] = useState({ cpsw: "" });
 
-    const [cpswMatch, setCpswMatch] = useState({cpsw: ""});
+    const [cpswMatch, setCpswMatch] = useState({ cpsw: "" });
 
-    const [allError, setAllError] = useState({all:""});
+    const [allError, setAllError] = useState({ all: "" });
 
     function handleChange(e) {
         const { name, value } = e.target;
@@ -61,21 +74,21 @@ function Register() {
             if (len === 0) error = "";
 
             else if (len <= 3 || len >= 16) error = "Το όνομά σας πρέπει να αποτελείται από 4 μέχρι 15 γράμματα";
-            
+
             else if (value.includes(" ")) error = "Το όνομά σας δεν μπορεί να περιέχει κενά";
-            
-            else if (regex.test(value)) error = "Το όνομά σας δεν επιτρέπεται να περιέχει ψηφία";    
+
+            else if (regex.test(value)) error = "Το όνομά σας δεν επιτρέπεται να περιέχει ψηφία";
         }
 
         if (name === "lname") {
             let len = value.length;
             let regex = /\d/;
             if (len === 0) error = "";
-            
+
             else if (len <= 3 || len >= 16) error = "Το επιθετό σας πρέπει να αποτελείται από 4 μεχρι 15 γράμματα";
-            
+
             else if (value.includes(" ")) error = "Το επιθετό σας δεν μπορεί να περιέχει κενά";
-            
+
             else if (regex.test(value)) error = "Το επιθετό σας δεν επιτρέπεται να περιέχει ψηφία";
         }
 
@@ -83,26 +96,53 @@ function Register() {
             let len = value.length;
             let regex = /^\d-\d{8}-\d{2}$/;
             if (len === 0) error = "";
-            
+
             else if (regex.test(value)) error = "";
-            
+
             else error = "Ο αριθμός πρέπει να είναι αυτής της μορφής χ-χχχχχχχχ-χχ";
+
+            axios.get(`http://localhost:5000/check_clock/${value}`)
+                .then((res) => {
+                    if (res.data.exists) {
+                        setErrors(prev => ({ ...prev, clock: "Υπάρχει ήδη αυτό το ρολόϊ." }));
+                    }
+                })
+                .catch((err) => console.log(err));
         }
 
         if (name === "username") {
             let len = value.length;
+
             if (len === 0) error = "";
-            
-            else if (len <= 4 || len >= 16) error = "Το username σας πρέπει να αποτελείται από 5 μέχρι 15 γράμματα";
-            
+
+            else if (len < 5 || len > 10) error = "Το username σας πρέπει να αποτελείται από 5 μέχρι 10 γράμματα";
+
             else if (value.includes(" ")) error = "Το username σας δεν μπορεί να περιέχει κενά";
+
+            axios.get(`http://localhost:5000/check_username/${value}`)
+                .then((res) => {
+                    if (res.data.exists) {
+                        setErrors(prev => ({ ...prev, username: "Υπάρχει χρήστης με αυτό το username." }));
+                    }
+                })
+                .catch((err) => console.log(err.message));
+        }
+
+        if (name === "email") {
+            let len = value.length;
+            if (len === 0) error = "";
+
+            axios.get(`http://localhost:5000/check_email/${value}`)
+                .then((res) => {
+                    if (res.data.exists) {
+                        setErrors(prev => ({ ...prev, email: "Υπάρχει ήδη αυτό το email." }));
+                    }
+                })
+                .catch((err) => console.log(err));
         }
 
         let password = formData.password;
         let confirm = conPass.cpsw;
-
-        // if (name === "password") password = value;
-        // if (name === "cpsw") confirm = value;
 
         if (name === "password") {
             password = value;
@@ -113,7 +153,7 @@ function Register() {
                 cpswerror = "";
                 cpswmatch = "Οι δύο κωδικοί ταιρίαζουν";
             }
-            else if(password !== confirm && cpswlen === 0){
+            else if (password !== confirm && cpswlen === 0) {
                 cpswerror = "";
                 cpswmatch = "";
             }
@@ -150,53 +190,46 @@ function Register() {
             }
         }
 
-        if (name !== "email" && name !== "provider") {
-            setErrors(prev => ({...prev, [name]: error,}));
+        if (name !== "provider") {
+            setErrors(prev => ({ ...prev, [name]: error}));
 
-            setCpswError(prev => ({...prev, cpsw: cpswerror}));
+            setCpswError(prev => ({ ...prev, cpsw: cpswerror }));
 
-            setCpswMatch(prev => ({...prev, cpsw: cpswmatch}));
+            setCpswMatch(prev => ({ ...prev, cpsw: cpswmatch }));
         }
     }
 
     function handleSubmit(e) {
         e.preventDefault();
-
         const hasErrors = Object.values(errors).some(err => err !== "");
         const hasCpswErrors = Object.values(cpswError).some(err => err !== "");
 
-        alert(hasErrors);
+        // alert(hasErrors);
         // true if one of them is wrong
-        alert(hasCpswErrors);
+        // alert(hasCpswErrors);
         // false if its empty
-        const s = [
-            ...Object.values(errors)
-        ]
-        
-        alert(s.join("\n"));
         if (hasErrors || hasCpswErrors) {
-            alert("some");
-
-            setAllError(prev => ({...prev, all: "Υπάρχουν λάθη στη φόρμα"}));
+            setAllError(prev => ({ ...prev, all: "Υπάρχουν λάθη στη φόρμα" }));
             return;
         }
-        console.log("Form submitted:", formData);
-        navigate('/match');
+
+        axios.post('http://localhost:5000/register', formData)
+            .then((res) => {
+                navigate('/match');
+                console.log(res);
+                console.log("Form submitted:", formData);
+            })
+            .catch((err) => console.log(err));
     }
 
     const [showPassword, setShowPassword] = useState(false);
     const [showConfPassword, setShowConfPassword] = useState(false);
-
-
     return (
         <div className={styles.container}>
-
             <div className={styles.header}>Register Form</div>
-
             <div className={styles.registerForm}>
                 <form onSubmit={handleSubmit}>
                     <p className={styles.titlee}>Πρέπει να συμπληρώσεται όλα τα πεδία της φόρμας</p><br />
-
                     Όνομα<span className={styles.must}>*</span>:<br /><input type="text" name="fname" value={formData.fname} required onChange={handleChange} className={errors.fname ? styles.inputError : ""} />
                     <div className={styles.errorMsg}>{errors.fname}</div><br />
 
@@ -208,13 +241,16 @@ function Register() {
 
                     Πάροχος ενέργειας<span className={styles.must}>*</span>:<br />
                     <select name="provider" required onChange={handleChange} value={formData.provider}>
-                        <option defaultValue="some">SOMDSO</option>
-                        <option value="zeniu">ZENIU</option>
-                        <option value="protergia">PROTERGIA</option>
-                        <option value="other">PROTERGIA</option>
+                        {providers.map((provider) => {
+                            return (
+                                <option key={provider.providerid} value={provider.providername}>
+                                    {provider.providername}
+                                </option>);
+                        })}
                     </select><br /><br />
 
-                    Email<span className={styles.must}>*</span>:<br /><input type="email" name="email" value={formData.email} required onChange={handleChange} /><br /><br />
+                    Email<span className={styles.must}>*</span>:<br /><input type="email" name="email" value={formData.email} required onChange={handleChange} className={errors.email ? styles.inputError : ""} />
+                    <div className={styles.errorMsg}>{errors.email}</div><br />
 
                     Username<span className={styles.must}>*</span>:<br /><input type="text" name="username" value={formData.username} required onChange={handleChange} className={errors.username ? styles.inputError : ""} />
                     <div className={styles.errorMsg}>{errors.username}</div><br />
@@ -233,13 +269,9 @@ function Register() {
                     <div className={styles.errorMsg}>{allError.all}</div>
                     <input type="submit" value="Εγγραφή" />
                 </form>
-
                 <p>Έχεις λογαριασμό; κάνε <Link to='/login'>Σύνδεση</Link></p>
-
             </div>
-
         </div>
     )
 }
-
 export default Register;
--- server/server.js
+++ server/server.js
@@ -24,6 +24,72 @@ app.get('/', (req, res)=>{
     res.send("Hello");
 });
 
+app.post('/register', (req, res)=>{
+    const sql = "INSERT INTO userss (fname, lname, clock, provider, email, username,  password) VALUES (?, ?, ?, ?, ?, ?, ?)";
+    const values = [
+        req.body.fname,
+        req.body.lname,
+        req.body.clock,
+        req.body.provider,
+        req.body.email,
+        req.body.username,
+        req.body.password
+    ];
+    db.query(sql, values, (err, result)=>{
+        if(err){
+            console.log(err.message);
+            return res.json({message:"Something happend with MySQL"});
+        }
+        // console.log(values);
+        return res.json({success:"Success"});
+    });
+});
+
+app.get('/check_username/:username', (req, res) => {
+    const sql = "SELECT username FROM userss WHERE username = ?";
+
+    const username = req.params.username;
+
+    db.query(sql, [username], (err, result) => {
+        if(err) return res.json(err);
+
+        return res.json({exists: result.length > 0});
+    });
+});
+
+app.get('/check_email/:email', (req, res)=>{
+    const sql = "SELECT email FROM userss WHERE email = ?";
+    const email = req.params.email;
+
+    db.query(sql, [email], (err, result) => {
+        if(err) return res.json(err);
+        return res.json({exists: result.length > 0});
+    });
+})
+
+app.get('/check_clock/:clock', (req, res) => {
+    const sql = "SELECT clock FROM userss WHERE clock = ?";
+    const clock = req.params.clock;
+
+    db.query(sql, [clock], (err, result) => {
+        if(err) return res.json(err);
+        return res.json({exists: result.length > 0});
+    });
+});
+
+app.get('/get_providers', (req, res)=>{
+    const sql = "SELECT * FROM providers";
+
+    db.query(sql, (err, result)=>{
+        if(err){
+            console.log(err.message);
+            return res.status(500).json({message:"Server error"});
+        }
+        console.log(result);
+        res.json(result);
+    });
+});
+
 app.listen(port, ()=>{
     console.log(`Listening to port ${port}`);
 });
