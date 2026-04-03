acb64648f0c85147fd233fb66a43b1188a7fe983
Panagiotis-Oikonomou <panosoikonomou04@gmail.com>
2026-04-03 11:54:35 +0300

    Added UpAdmin to AddParoxo and ManageParoxo

--- client/src/elements/AddParoxo.jsx
+++ client/src/elements/AddParoxo.jsx
@@ -1,25 +1,22 @@
 import React, { useState } from "react";
 import axios from 'axios';
-import { Link, useNavigate, useNavigation } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import styles from './AddParoxo.module.css';
 
-import paroxoi from '../assets/images/paroxoi.png';
-import users from '../assets/images/users.png';
-import notifications from '../assets/images/notifications.png';
-import profile from '../assets/images/profile.png';
-import menu from '../assets/images/menu.png';
+import { UpAdmin } from "../components/UpAdmin";
 
 
 function AddParoxo() {
     return (
         <div className={styles.container}>
-            <img src={menu} className={styles.menu} />
+            <UpAdmin></UpAdmin>
+            {/* <img src={menu} className={styles.menu} />
             <div className={styles.up}>
                 <Link to='/paroxoi'><img src={paroxoi} /></Link>
                 <Link to='/users'><img src={users} /></Link>
                 <Link to='/notifications/admin'><img src={notifications} /></Link>
                 <Link to='/profile/admin'><img src={profile} /></Link>
-            </div>
+            </div> */}
 
             <div className={styles.paroxos}>
                 <p className={styles.titlee}>Δημιουργία καινούργιας περιοχής</p><br/>
