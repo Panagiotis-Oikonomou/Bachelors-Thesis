b5f85449991d0c05c2439bf6c133926d3dc817e0
Panagiotis-Oikonomou <panosoikonomou04@gmail.com>
2026-04-22 17:17:43 +0300

    Moved area route logic to different files

---
+++ server/routes/areaRoutes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const area = require('../controllers/areaController');
+
+router.post("/addArea", area.addArea);
+
+module.exports = router;
