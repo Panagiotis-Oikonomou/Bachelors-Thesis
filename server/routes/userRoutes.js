060e980fe342174e119159cca1e06dfb32dc4fe1
Panagiotis-Oikonomou <panosoikonomou04@gmail.com>
2026-04-22 16:55:01 +0300

    moved userRoute logic to different file

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const user = require('../controllers/userController');
+
+router.post("/register", user.register);
+router.post("/login", user.login);
+router.get("/:id", user.getProfile);
+router.put("/:id", user.updateUser);
+
+module.exports = router;
