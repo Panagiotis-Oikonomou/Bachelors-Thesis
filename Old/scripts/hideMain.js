748512c10b7c4cd8778d84d8a2de640709c30210
Panagiotis-Oikonomou <panosoikonomou04@gmail.com>
2026-06-09 10:14:30 +0300

    Stop tracking Old folder

--- Old/scripts/hideMain.js
+++
@@ -1,6 +0,0 @@
-const menu = document.querySelector('.menu');
-const container = document.querySelector('.container');
-
-menu.addEventListener('click', () => {
-    container.classList.toggle('main-hidden');
-});
--- Old/scripts/openMap.js
+++
@@ -1,6 +0,0 @@
-const map_button = document.querySelector('.map-button');
-const cont = document.querySelector('.container');
-
-map_button.addEventListener('click', () => {
-    cont.classList.toggle('main-move');
-});
