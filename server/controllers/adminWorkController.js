ddec1eb661c29c70733f30c11e2458dfa7975da0
Panagiotis-Oikonomou <panosoikonomou04@gmail.com>
2026-06-20 10:21:32 +0300

    Deleting all the data the user had when he is deleted.

--- server/controllers/adminWorkController.js
+++ server/controllers/adminWorkController.js
@@ -2,7 +2,7 @@ const db = require('../config/db');
 
 exports.getUsers = async (req, res) => {
     try {
-        const sql = "SELECT userid, fname, lname, clock, provider, email, username FROM users";
+        const sql = "SELECT userid, fname, lname, clock, provider, email, username FROM users ORDER BY username";
 
         const [rows] = await db.query(sql);
         res.json(rows);
@@ -15,8 +15,15 @@ exports.getUsers = async (req, res) => {
 exports.deleteUser = async (req, res) => {
     try{
         const sql = "DELETE FROM users WHERE userid = ?";
+        const sqlArea = "DELETE FROM areas WHERE userid = ?";
+        const sqlCriteria = "DELETE FROM criterias WHERE userid = ?";
+        const sqlNotification = "DELETE FROM notifications WHERE userid = ?";
         const userid = req.params.userid;
+
         await db.query(sql, [userid]);
+        await db.query(sqlArea, [userid]);
+        await db.query(sqlCriteria, [userid]);
+        await db.query(sqlNotification, [userid]);
         return res.sendStatus(200);
     }
     catch(err){
@@ -27,8 +34,15 @@ exports.deleteUser = async (req, res) => {
 exports.deleteProvider = async (req, res) => {
     try{
         const sql = "DELETE FROM providers WHERE providerid = ?";
+        const sqlProviderId = "SELECT providername FROM providers WHERE providerid = ?";
+        const sqlUpdate = "UPDATE users SET provider = '' WHERE provider = ?";
         const id = req.params.providerid;
+
+        const [result] =  await db.query(sqlProviderId, [id]);
+        if (result.length === 0) return res.sendStatus(404);
+
         await db.query(sql, [id]);
+        await db.query(sqlUpdate, [result[0].providername]);
         return res.sendStatus(200);
     }
     catch(err){
